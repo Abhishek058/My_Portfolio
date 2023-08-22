6820f7feb7bf8297a6b1e87b4e15835e2f53ead4
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-08-22 13:13:48 +0530

    theme button added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*-----------Navbar Script-------------- */
+
 const ele = document.getElementById("hamburger");
 const nav = document.getElementsByClassName("nav-div")[0]; // Assuming you want to select the first element with the class 'nav-div'
 
@@ -6,5 +8,24 @@ ele.addEventListener("click", () => {
     nav.style.right = "-1000px";
   } else {
     nav.style.right = "0px";
+  }
+});
+
+/*-------------Theme Changing Script----------*/
+
+const themeToggle = document.getElementById("theme-toggle");
+const themeLink = document.getElementById("theme-link");
+let isDarkMode = false;
+
+themeToggle.addEventListener("click", () => {
+  isDarkMode = !isDarkMode;
+  if (isDarkMode) {
+    themeLink.href = "dark.css";
+    themeToggle.innerHTML = "🌙";
+    document.body.style.transition = "2s";
+  } else {
+    themeLink.href = "index.css";
+    themeToggle.innerHTML = "☀️";
+    document.body.style.transition = "2s";
   }
 });
